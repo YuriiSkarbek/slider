5c705f008b191538fdc75817fff291296c725f85
Yurii Skarbek <yurii.skarbek@gmail.com>
2018-10-10 00:58:37 +0300

    Added automatically change slides; Added some comments for function; Fixed moving in a circle;

--- scripts/slider.js
+++ scripts/slider.js
@@ -1,10 +1,12 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+// Change current slide, press the "arrow button"
 function plusSlides(n) {
-    showSlides(slideIndex += 1)
+    showSlides(slideIndex += n)
 }
 
+// Change current slide, press the "dot button" 
 function currentSlide(n) {
     showSlides(slideIndex = n);
 }
@@ -18,7 +20,7 @@ function showSlides(n) {
     }
 
     if (n < 1) {
-        slideIndex = slid.length;
+        slideIndex = slides.length;
     }
     for (var i = 0; i < slides.length; i++) {
         slides[i].style.display = 'none';
@@ -30,3 +32,6 @@ function showSlides(n) {
     slides[slideIndex - 1].style.display = 'block';
     dots[slideIndex - 1].className += " active";
 }
+
+//auto change slides
+setInterval(function(){showSlides(slideIndex += 1)},7300);
